b4e79efb25fffc616ce16eb178f14a1446582472
H3xept <h3xept@gmail.com>
2017-12-23 15:24:52 +0100

    [*] XCWorkspace (See additional notes) StomtSDK.workspace for test app and SDK development

---
+++ Example/iOS Example App/iOS Example App/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  iOS Example App
+//
+//  Created by Leonardo Cascianelli on 20/12/2017.
+//  Copyright © 2017 stomt. All rights reserved.
+//
+
+import UIKit
+import Stomt
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Set the stomt API mode - Default .testAPI
+        Stomt.Shared.baseAPI = .productionAPI
+        // ===================
+    }
+    
+    @IBAction func creationPressed(_ button: UIButton) {
+        // Create a new stomt using the creation widget.
+        // Call can throw - See Errors section in the README
+        do {
+            try Stomt.creationWidget(withTargetID: "stomt", fromViewController: self)
+        } catch {
+            print("\(error)")
+        }
+        
+    }
+    
+    @IBAction func feedPressed(_ button: UIButton) {
+        // Retrieve an existing feed using the feed widget.
+        // Call can throw - See Errors section in the README
+        do {
+            try Stomt.feedWidget(withTargetID: "stomt", fromViewController: self)
+        } catch {
+            print("\(error)")
+        }
+        
+    }
+    
+}
